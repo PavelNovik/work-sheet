acd55193a01938fbeebc8697f01b3912f411848b
Pavel Novik <brn2befree@gmail.com>
2023-09-15 16:58:55 +0300

    button commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,19 @@ import './App.css';
 import {NewComponent} from "./NewComponent";
 
 function App() {
+
+    // const myFirstSubscriber = (event: React.MouseEvent<HTMLButtonElement>) => {
+    //     console.log(event)
+    // }
+    const onClickHandler = (name: string) => {
+console.log(name)
+    }
     return (
         <div className="App">
-            <NewComponent/>
+            {/*<NewComponent/>*/}
+            <button onClick={() => onClickHandler("name")}>First btn</button>
+            {/*<button onClick={myFirstSubscriber}>Second btn</button>*/}
+            {/*<button onClick={(event) => {console.log("hello")}}>Second btn</button>*/}
         </div>
     );
 }
